bb57c6a3c99cfd4a2207fe1686327a3cd84d0966
Natanael LR <nat.contactme@gmail.com>
2025-06-18 03:44:55 -0600

    eliminando vh de css y componente para ver si se arregla

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google";
 import { GrainOverlay } from "@/components/GrainOverlay";
 import "./globals.css";
 import clsx from "clsx";
-import VhFixer from "@/components/VhFixer";
 
 const inter = Inter({
   weight: ["400", "500", "600", "700", "800", "900"],
@@ -53,7 +52,7 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-    <VhFixer />
+
 
   return (
     <html lang="en">

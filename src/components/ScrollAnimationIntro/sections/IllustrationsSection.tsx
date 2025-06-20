85141ecaae1f0cbaff51a692f89881df008b3ae5
Natanael LR <nat.contactme@gmail.com>
2025-06-20 07:10:56 -0600

    a ver si asi no se traba en mobile

--- src/components/ScrollAnimationIntro/sections/IllustrationsSection.tsx
+++ src/components/ScrollAnimationIntro/sections/IllustrationsSection.tsx
@@ -31,28 +31,20 @@ export default function IllustrationsSection({ scrollYProgress }: SectionProps) 
   const illustrationsImageOpacity = useTransform(scrollYProgress, [0.225, 0.25, 0.3, 0.325], [0, 0.5, 0.5, 0])
 
   const layer1Y = useTransform(scrollYProgress, [0.225, 0.325], isMobile ? [-50, -400] : [-100, -870])
-  const layer1Scale = useTransform(scrollYProgress, [1.225, 1.25, 1.3, 1.325], isMobile ? [1.4, 1.45, 1.45, 1.4] : [1.74, 1.79, 1.79, 1.74])
-
+  
   const layer2Y = useTransform(scrollYProgress, [0.225, 0.325], isMobile ? [200, -600] : [300, -860])
-  const layer2Scale = useTransform(scrollYProgress, [1.225, 1.25, 1.3, 1.325], isMobile ? [1.6, 1.65, 1.65, 1.6] : [1.72, 1.77, 1.77, 1.72])
-
+ 
   const layer3Y = useTransform(scrollYProgress, [0.225, 0.325], isMobile ? [-50, -500] : [-100, -1000])
-  const layer3Scale = useTransform(scrollYProgress, [1.225, 1.25, 1.3, 1.325], isMobile ? [1.6, 1.65, 1.65, 1.6] : [1.74, 1.79, 1.79, 1.74])
-
+ 
   const layer4Y = useTransform(scrollYProgress, [0.225, 0.325], isMobile ? [-100, -450] : [-200, -700])
-  const layer4Scale = useTransform(scrollYProgress, [1.225, 1.25, 1.3, 1.325], isMobile ? [1.62, 1.67, 1.67, 1.62] : [1.76, 1.81, 1.81, 1.76])
-
+  
   const layer5Y = useTransform(scrollYProgress, [0.225, 0.325], isMobile ? [0, -400] : [0, -700])
-  const layer5Scale = useTransform(scrollYProgress, [1.225, 1.25, 1.3, 1.325], isMobile ? [1.65, 1.7, 1.7, 1.65] : [1.78, 1.83, 1.83, 1.78])
-
+ 
   const layer6Y = useTransform(scrollYProgress, [0.225, 0.325], isMobile ? [-50, -350] : [-100, -650])
-  const layer6Scale = useTransform(scrollYProgress, [1.425, 1.45, 1.5, 1.525], isMobile ? [1.9, 1.95, 1.95, 1.9] : [2.0, 2.05, 2.05, 2.0])
-
+ 
   const layer7Y = useTransform(scrollYProgress, [0.225, 0.325], isMobile ? [-50, -200] : [0, -500])
-  const layer7Scale = useTransform(scrollYProgress, [1.225, 1.25, 1.3, 1.325], isMobile ? [1.7, 1.75, 1.75, 1.7] : [1.82, 1.87, 1.87, 1.82])
-
+ 
   const layer8Y = useTransform(scrollYProgress, [0.225, 0.325], isMobile ? [0, -150] : [0, -400])
-  const layer8Scale = useTransform(scrollYProgress, [1.225, 1.25, 1.3, 1.325], isMobile ? [1.72, 1.77, 1.77, 1.72] : [1.84, 1.89, 1.89, 1.84])
 
   return (
     <motion.div
